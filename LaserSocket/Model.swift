868a28e24ed065fb7b7ec9122ff32b3623d443a8
Serge Urakhchin <urakhchin@gmail.com>
2024-02-16 17:54:56 -0600

    Initial Commit

---
+++ LaserSocket/Model.swift
@@ -0,0 +1,14 @@
+import Foundation
+import RealityKit
+
+struct Model {
+    var modelName: String
+
+    // Add other properties as needed
+
+    var modelEntity: ModelEntity? {
+        // Implement the logic to load ModelEntity based on modelName
+        // Return the loaded ModelEntity or nil if loading fails
+        return nil
+    }
+}
---
+++ LaserSocket/SocketConnection.swift
@@ -0,0 +1,8 @@
+//
+//  SocketConnection.swift
+//  LaserSocket
+//
+//  Created by Sergey Urakhchin on 2/9/24.
+//
+
+import Foundation
