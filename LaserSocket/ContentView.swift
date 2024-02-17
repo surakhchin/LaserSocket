1c0d2fb861e0a9e7f2f85d7ae4b909932d3d73dc
Serge Urakhchin <urakhchin@gmail.com>
2024-02-17 16:19:30 -0600

    got the blue box to position at global anchor

--- LaserSocket/ContentView.swift
+++ LaserSocket/ContentView.swift
@@ -16,17 +16,19 @@ struct ContentView: View {
     @State private var isPlacementEnabled = false
     @State private var selectedModel: Model?
     @State private var modelConfirmedForPlacement: Model?
-    
+
+    // Add the blueBoxAdded flag
+    @State private var blueBoxAdded = false
 
     private var models: [Model] = {
         //Dynamically get our model filenames
         let filemanger = FileManager.default
-        
+
         guard let path = Bundle.main.resourcePath,
               let files = try? filemanger.contentsOfDirectory(atPath: path) else {
             return []
         }
-        
+
         var availableModels: [Model] = []
         for filename in files where filename.hasSuffix("usdz") {
             let modelName = filename.replacingOccurrences(of: ".usdz", with: "")
@@ -38,7 +40,7 @@ struct ContentView: View {
 
     var body: some View {
         ZStack(alignment: .bottom) {
-            ARViewContainer(modelConfirmedForPlacement: self.$modelConfirmedForPlacement)
+            ARViewContainer(modelConfirmedForPlacement: self.$modelConfirmedForPlacement, blueBoxAdded: $blueBoxAdded)
             if self.isPlacementEnabled {
                 PlacementButtonsView(isPlacementEnabled: self.$isPlacementEnabled, selectedModel: self.$selectedModel, modelConfirmedForPlacement: self.$modelConfirmedForPlacement)
             } else {
@@ -50,22 +52,43 @@ struct ContentView: View {
 
 struct ARViewContainer: UIViewRepresentable {
     @Binding var modelConfirmedForPlacement: Model?
-    
+    @Binding var blueBoxAdded: Bool // Add the blueBoxAdded binding
+
     func makeUIView(context: Context) -> ARView {
         let arView = CustomARView(frame: .zero)
         return arView
     }
-    
+
     func updateUIView(_ uiView: ARView, context: Context) {
+        if !blueBoxAdded {
+            // Create a global anchor
+            let globalAnchorEntity = AnchorEntity(world: [0.5, 0, -1.5]) // Adjust x, y, z as needed
+            uiView.scene.addAnchor(globalAnchorEntity)
+
+            // Create a blue box and attach it to the global anchor
+            let blueBox = MeshResource.generateBox(size: 0.1)
+            let blueBoxEntity = ModelEntity(mesh: blueBox, materials: [SimpleMaterial(color: .blue, isMetallic: false)])
+            let blueBoxAnchorEntity = AnchorEntity(world: [0, 0, 0]) // Set the world position as needed
+            blueBoxAnchorEntity.addChild(blueBoxEntity)
+            globalAnchorEntity.addChild(blueBoxAnchorEntity)
+
+            print("Debug: Initial blue box added to scene")
+
+            blueBoxAdded = true
+        }
+
         if let model = self.modelConfirmedForPlacement {
             if let modelEntity = model.modelEntity {
+                // Create an anchor for the current model
+                let currentAnchorEntity = AnchorEntity(plane: .any)
+                currentAnchorEntity.addChild(modelEntity)
+                uiView.scene.addAnchor(currentAnchorEntity)
+
                 print("Debug: adding model to scene - \(model.modelName)")
-                let anchorEntity = AnchorEntity(plane: .any)
-                anchorEntity.addChild(modelEntity)
-                uiView.scene.addAnchor(anchorEntity)
             } else {
                 print("Debug: unable to load modelEntity for - \(model.modelName)")
             }
+
             DispatchQueue.main.async {
                 modelConfirmedForPlacement = nil
             }
@@ -75,7 +98,7 @@ struct ARViewContainer: UIViewRepresentable {
 
 class CustomARView: ARView {
     let focusSquare = FESquare()
-    
+
     required init(frame frameRect: CGRect) {
         super.init(frame: frameRect)
         focusSquare.viewDelegate = self
@@ -83,11 +106,11 @@ class CustomARView: ARView {
         focusSquare.setAutoUpdate(to: true)
         setupARView()
     }
-    
+
     @MainActor required dynamic init?(coder decoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     func setupARView() {
         let config = ARWorldTrackingConfiguration()
         config.planeDetection = [.horizontal, .vertical]
@@ -103,7 +126,7 @@ extension CustomARView: FEDelegate {
     func toTrackingState() {
         print("tracking")
     }
-    
+
     func toInitializingState() {
         print("initializing")
     }
@@ -113,7 +136,7 @@ struct PlacementButtonsView: View {
     @Binding var isPlacementEnabled: Bool
     @Binding var selectedModel: Model?
     @Binding var modelConfirmedForPlacement: Model?
-    
+
     var body: some View {
         HStack {
             // Cancel Button
@@ -153,23 +176,23 @@ struct PlacementButtonsView: View {
 struct ModelPickerView: View {
     @Binding var isPlacementEnabled: Bool
     @Binding var selectedModel: Model?
-    
+
     var models: [Model]
-    
+
     var body: some View {
         ScrollView(.horizontal, showsIndicators: false) {
             HStack(spacing: 30) {
                 ForEach(0 ..< self.models.count) { index in
                     Button(action: {
                         print("DEBUG: selected model with name: \(self.models[index].modelName)")
-                        
+
                         self.selectedModel = self.models[index]
                         self.isPlacementEnabled = true
                     }) {
                         Image(uiImage: self.models[index].image)
                             .resizable()
                             .frame(height: 80)
-                            .aspectRatio(1, contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                            .aspectRatio(1, contentMode: .fill)
                             .background(Color.white)
                             .cornerRadius(12)
                     }
